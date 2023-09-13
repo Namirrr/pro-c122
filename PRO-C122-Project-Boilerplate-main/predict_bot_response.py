66a4b78146d8b49523728881134f8ae885bee28c
Namirrr <103450124+Namirrr@users.noreply.github.com>
2023-09-13 17:53:08 +0100

    Update predict_bot_response.py

--- PRO-C122-Project-Boilerplate-main/predict_bot_response.py
+++ PRO-C122-Project-Boilerplate-main/predict_bot_response.py
@@ -1,11 +1,10 @@
-# Text Data Preprocessing Lib
+#Text Data Preprocessing Lib
 import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
 
 
-from nltk.stem import PorterStemmer
-stemmer = PorterStemmer()
+ignore_words = ['?', '!',',','.', "'s", "'m"]
 
 import json
 import pickle
@@ -13,105 +12,58 @@ import pickle
 import numpy as np
 import random
 
+# Model Load Lib
+import tensorflow
+from data_preprocessing import get_stem_words
 
-words=[] #list of unique roots words in the data
-classes = [] #list of unique tags in the data
-pattern_word_tags_list = [] #list of the pair of (['words', 'of', 'the', 'sentence'], 'tags')
+model = tensorflow.keras.models.load_model('./static/assets/model_files/chatbot_model.h5')
 
-ignore_words = ['?', '!',',','.', "'s", "'m"]
-
-train_data_file = open('./static/assets/chatbot_corpus/intents.json').read()
-intents = json.loads(train_data_file)
-
-def get_stem_words(words, ignore_words):
-    stem_words = []
-    for word in words:
-        if word not in ignore_words:
-            w = stemmer.stem(word.lower())
-            stem_words.append(w)
-    return stem_words
- 
-def create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words):
-
-    for intent in intents['intents']:
-
-        # Add all patterns and tags to a list
-        for pattern in intent['patterns']:            
-            pattern_word = nltk.word_tokenize(pattern)            
-            words.extend(pattern_word)                        
-            pattern_word_tags_list.append((pattern_word, intent['tag']))
-              
-    
-        # Add all tags to the classes list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
-            
-    stem_words = get_stem_words(words, ignore_words) 
-    stem_words = sorted(list(set(stem_words)))
-    classes = sorted(list(set(classes)))
-
-    return stem_words, classes, pattern_word_tags_list
+# Load data files
+intents = json.loads(open('./static/assets/chatbot_corpus/intents.json').read())
+words = pickle.load(open('./static/assets/chatbot_corpus/words.pkl','rb'))
+classes = pickle.load(open('./static/assets/chatbot_corpus/classes.pkl','rb'))
 
 
-# Training Dataset: 
-# Input Text----> as Bag of Words 
-# Tags-----------> as Label
+def preprocess_user_input(user_input):
 
-def bag_of_words_encoding(stem_words, pattern_word_tags_list):
-    
-    bag = []
-    for word_tags in pattern_word_tags_list:
-        # example: word_tags = (['hi', 'there'], 'greetings']
+    input_word_token_1 = nltk.word_tokenize(user_input)
+    input_word_token_2 = get_stem_words(input_word_token_1, ignore_words) 
+    input_word_token_2 = sorted(list(set(input_word_token_2)))
 
-        pattern_words = word_tags[0] # ['hi', 'there']
-        stem_pattern_words = get_stem_words(pattern_words, ignore_words)
-        bag_of_words = []
-
-        # Input data encoding 
-        for word in stem_words:            
-            if word in stem_pattern_words:              
-                bag_of_words.append(1)
-            else:
-                bag_of_words.append(0)
-    
-        bag.append(bag_of_words)
-    
-    return np.array(bag)
-
-def class_label_encoding(classes, pattern_word_tags_list):
-    
-    labels = []
-
-    for word_tags in pattern_word_tags_list:
-
-        # Start with list of 0s
-        labels_encoding = list([0]*len(classes))  
-
-        # example: word_tags = (['hi', 'there'], 'greetings']
-
-        tag = word_tags[1]   # 'greetings'
-
-        tag_index = classes.index(tag)
-
-        # Labels Encoding
-        labels_encoding[tag_index] = 1
-
-        labels.append(labels_encoding)
-        
-    return np.array(labels)
-
-def preprocess_train_data():
+    bag=[]
+    bag_of_words = []
+   
+    # Input data encoding 
+    for word in words:            
+        if word in input_word_token_2:              
+            bag_of_words.append(1)
+        else:
+            bag_of_words.append(0) 
+    bag.append(bag_of_words)
   
-    stem_words, tag_classes, word_tags_list = create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words)
+    return np.array(bag)
     
-    # Convert Stem words and Classes to Python pickel file format
-    pickle.dump(stem_words, open('words.pkl','wb'))
-    pickle.dump(tag_classes, open('classes.pkl','wb'))
+def bot_class_prediction(user_input):
+    inp = preprocess_user_input(user_input)
+  
+    prediction = model.predict(inp)
+   
+    predicted_class_label = np.argmax(prediction[0])
+    
+    return predicted_class_label
 
-    train_x = bag_of_words_encoding(stem_words, word_tags_list)
-    train_y = class_label_encoding(tag_classes, word_tags_list)
-    
-    return train_x, train_y
 
-# preprocess_train_data()
+def bot_response(user_input):
+
+   predicted_class_label =  bot_class_prediction(user_input)
+ 
+   predicted_class = classes[predicted_class_label]
+
+
+   for intent in intents['intents']:
+      
+    if intent['tag']==predicted_class:
+       
+        bot_response = random.choice(intent['responses'])
     
+        return bot_response
